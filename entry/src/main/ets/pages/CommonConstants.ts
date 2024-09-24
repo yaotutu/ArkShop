391f60d9fc3424398f36db0289b31061cb067ac2
yaotutu <957810668@qq.com>
2024-09-24 12:19:58 +0800

    ✨ (entry/src): add CommonConstants class to define common constants for input length, padding, and titles to improve code maintainability and readability. 🌐 (localization): add English and Chinese translations for various strings to support internationalization of the application. 🗂 (resources): add media and configuration files to enhance the application’s visual and structural components across different modules.

---
+++ entry/src/main/ets/pages/CommonConstants.ts
@@ -0,0 +1,69 @@
+/**
+ * 所有功能的通用常量。
+ */
+export default class CommonConstants {
+  /**
+   * 账号的输入长度。
+   */
+  static readonly INPUT_ACCOUNT_LENGTH = 11;
+
+  /**
+   * 密码的输入长度。
+   */
+  static readonly INPUT_PASSWORD_LENGTH = 8;
+
+  /**
+   * 输入框的左填充。
+   */
+  static readonly INPUT_PADDING_LEFT = 0;
+
+  /**
+   * 模拟登录的延迟时间。
+   */
+  static readonly LOGIN_DELAY_TIME = 2000;
+
+  /**
+   * 组件的通用间距。
+   */
+  static readonly COMMON_SPACE = 12;
+
+  /**
+   * 首页的标题文本。
+   */
+  static readonly HOME_TITLE = '首页';
+
+  /**
+   * 设置页面的标题文本。
+   */
+  static readonly MINE_TITLE = '我的';
+
+  /**
+   * 其他登录方法的间距。
+   */
+  static readonly LOGIN_METHODS_SPACE = 44;
+
+  /**
+   * 组件的宽度或高度覆盖父组件。
+   */
+  static readonly FULL_PARENT = '100%';
+
+  /**
+   * 按钮的宽度。
+   */
+  static readonly BUTTON_WIDTH = '90%';
+
+  /**
+   * 设置列表的宽度。
+   */
+  static readonly SET_LIST_WIDTH = '42%';
+
+  /**
+   * 首页标签索引。
+   */
+  static readonly HOME_TAB_INDEX = 0;
+
+  /**
+   * 我的设置也没标签索引。
+   */
+  static readonly MINE_TAB_INDEX = 1;
+}
